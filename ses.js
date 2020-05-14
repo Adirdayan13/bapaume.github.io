facaa12c33999d69cdebbf788a46210f69a27473
Adir Dayan <adirdayan@gmail.com>
2020-05-14 10:56:19 +0200

    ses - east

--- ses.js
+++ ses.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === "production") {
 const ses = new aws.SES({
   accessKeyId: secrets.AWS_KEY,
   secretAccessKey: secrets.AWS_SECRET,
-  region: "eu-east-1"
+  region: "us-east-1"
 });
 
 exports.sendEmail = function(recipient, name, message, senderEmail, phone) {
