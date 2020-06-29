c8d65244d77c7282e9b1ff25936e7e179a5dd610
Adir Dayan <adirdayan@gmail.com>
2020-06-29 15:59:05 +0200

    Updates

---
+++ precache-manifest.fe9b6f3d65812a9e10e41a819a156ba3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2f3bd53135088b00471b8c8da5ccaa",
+    "url": "/adirdayan.me/index.html"
+  },
+  {
+    "revision": "134b1c449075acd89248",
+    "url": "/adirdayan.me/static/css/2.b0a38356.chunk.css"
+  },
+  {
+    "revision": "3776ee737db3f9dffdb2",
+    "url": "/adirdayan.me/static/css/main.a211adc0.chunk.css"
+  },
+  {
+    "revision": "134b1c449075acd89248",
+    "url": "/adirdayan.me/static/js/2.f4f011ee.chunk.js"
+  },
+  {
+    "revision": "a929e4c35621369695e0145e8abf42af",
+    "url": "/adirdayan.me/static/js/2.f4f011ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3776ee737db3f9dffdb2",
+    "url": "/adirdayan.me/static/js/main.9ddf202d.chunk.js"
+  },
+  {
+    "revision": "c35c3bb5c935ed17833e",
+    "url": "/adirdayan.me/static/js/runtime-main.fce9ddae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bapaume/precache-manifest.9a91d41b8ef708b1a1e6cfb1058ca3d5.js"
+  "/adirdayan.me/precache-manifest.fe9b6f3d65812a9e10e41a819a156ba3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bapaume/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adirdayan.me/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
