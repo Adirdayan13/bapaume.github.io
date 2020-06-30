371b651e9531678a43db38fe04efdf1233f2b0af
Adir Dayan <adirdayan@gmail.com>
2020-06-30 18:08:29 +0200

    Updates

---
+++ precache-manifest.9a91d41b8ef708b1a1e6cfb1058ca3d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b09786ab85f232e0302be65702d016e1",
+    "url": "/bapaume/index.html"
+  },
+  {
+    "revision": "134b1c449075acd89248",
+    "url": "/bapaume/static/css/2.b0a38356.chunk.css"
+  },
+  {
+    "revision": "1f0d91be1e06b8b68fe1",
+    "url": "/bapaume/static/css/main.a211adc0.chunk.css"
+  },
+  {
+    "revision": "134b1c449075acd89248",
+    "url": "/bapaume/static/js/2.f4f011ee.chunk.js"
+  },
+  {
+    "revision": "a929e4c35621369695e0145e8abf42af",
+    "url": "/bapaume/static/js/2.f4f011ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f0d91be1e06b8b68fe1",
+    "url": "/bapaume/static/js/main.db5ccd30.chunk.js"
+  },
+  {
+    "revision": "f855b025ea0939fb96b0",
+    "url": "/bapaume/static/js/runtime-main.b62e291f.js"
+  }
+]);
